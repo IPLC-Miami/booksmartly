471a415a9f820a1d7dc067c55199c46ab0c85ae4
pdarleyjr <pdarleyjr@gmail.com>
2025-05-31 18:51:38 -0400

    Fix Vite environment variable loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/cureit",
-  plugins: [react(), eslint()],
-});
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default ({ mode }) => {
+  // Load env files (e.g., .env, .env.production)
+  const env = loadEnv(mode, process.cwd(), '');
+  return defineConfig({
+    base: '/',
+    plugins: [react()],
+    define: {
+      'process.env': {
+        VITE_SUPABASE_URL: JSON.stringify(env.VITE_SUPABASE_URL),
+        VITE_SUPABASE_ANON_KEY: JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
+      }
+    }
+  });
+};
