fba464121fbe49311eeb0b3138314cc2ed6b4e5f
pdarleyjr <pdarleyjr@gmail.com>
2025-06-14 10:40:06 -0400

    Fix login timeout: Pass userId prop to ReceptionDashboard component

--- frontend/src/pages/ReceptionDashboardPage.jsx
+++ frontend/src/pages/ReceptionDashboardPage.jsx
@@ -1,9 +1,12 @@
 import ReceptionDashboard from "../components/ReceptionDashboard/ReceptionDashboard";
+import { useAuthContext } from "../utils/ContextProvider";
 
 function ReceptionDashboardPage() {
+  const { user } = useAuthContext();
+
   return (
     <div className="mb-24 mt-12 flex flex-col overflow-hidden p-4 font-noto md:px-12 md:py-8">
-      <ReceptionDashboard />
+      <ReceptionDashboard userId={user?.id} />
     </div>
   );
 }
