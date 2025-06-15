96bba864270cefa45e7d4f37a9ebdb439fcb09b5
pdarleyjr <pdarleyjr@gmail.com>
2025-06-15 16:09:24 -0400

    Update chatbot to use PORT environment variable

--- ml_model/chatbot/main1.py
+++ ml_model/chatbot/main1.py
@@ -162,4 +162,6 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8003)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
