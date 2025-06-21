2f149cb75a8a304bdd5ab4e673695f6d3cee06de
pdarleyjr <pdarleyjr@gmail.com>
2025-06-21 15:40:52 -0400

    Revert graphql schema implementation

--- backend/schema/schema.js
+++
@@ -1,8 +0,0 @@
-const { GraphQLSchema, GraphQLObjectType } = require("graphql");
-const Query = require("./Query");
-const Mutation = require("./mutations");
-
-module.exports = new GraphQLSchema({
-  query: Query,
-  mutation: Mutation,
-});
