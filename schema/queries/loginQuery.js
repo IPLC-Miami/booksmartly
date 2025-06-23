7f249e33fced09ae5c3a776dc4ce2d3bceb7b7fe
pdarleyjr <pdarleyjr@gmail.com>
2025-06-23 18:27:42 -0400

    fix(backend): Correct path to createTokens module in loginQuery

--- schema/queries/loginQuery.js
+++ schema/queries/loginQuery.js
@@ -3,7 +3,7 @@ const AuthType = require("../types/AuthType");
 const Client = require("../../models/client");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const createTokens = require("../../createTokens");
+const createTokens = require("../../backend/utils/createTokens.js");
 const { UserInputError } = require("apollo-server-express");
 
 
