300e4ea51a9794053da509f22315756aaf900c74
pdarleyjr <pdarleyjr@gmail.com>
2025-06-23 18:08:51 -0400

    fix(backend): Correct path to createTokens module

--- schema/mutations/registerClientMutation.js
+++ schema/mutations/registerClientMutation.js
@@ -6,7 +6,7 @@ const { UserInputError } = require("apollo-server-express");
 const {
   validateRegistration,
 } = require("../../validation/validateRegistration");
-const createTokens = require("../../createTokens");
+const createTokens = require("../../backend/util/createTokens");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
