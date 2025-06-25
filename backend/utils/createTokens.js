d0f0f30a915a5297424e6934c5a37ea80f2296c8
pdarleyjr <pdarleyjr@gmail.com>
2025-06-25 19:28:04 -0400

    feat: add createTokens utility

--- backend/utils/createTokens.js
+++ backend/utils/createTokens.js
@@ -2,16 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const createTokens = (user) => {
   const refreshToken = jwt.sign(
-    { userId: user.id, count: user.tokenCount },
-    process.env.JWT_REFRESH_SECRET_KEY,
+    { userId: user.id, count: user.count },
+    process.env.JWT_REFRESH_SECRET,
     {
       expiresIn: "7d",
     }
   );
-
   const accessToken = jwt.sign(
-    { userId: user.id },
-    process.env.JWT_ACCESS_SECRET_KEY,
+    { userId: user.id, count: user.count },
+    process.env.JWT_SECRET_KEY,
     {
       expiresIn: "15min",
     }
